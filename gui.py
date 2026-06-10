b3c25ff8086d57e954a3e401e1525aa226963807
qluu8 <116929783+qluu8@users.noreply.github.com>
2026-06-09 17:01:11 -0700

    Initial GUI for Inventory Tracking App

---
+++ gui.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkinter import ttk
+from database import get_connection
+
+def runGUI():
+    root = tk.Tk()
+
+    root.title("Inventory Tracking App")
+    root.geometry("600x400")
+
+    button_frame = tk.Frame(root)
+    button_frame.pack(side="top", fill="x")
+
+    addButton = tk.Button(button_frame, text="Add Card")
+    addButton.pack(side="left", padx="5", pady="5")
+
+    deleteButton = tk.Button(button_frame, text="Delete Card")
+    deleteButton.pack(side="left", padx="5", pady="5")
+
+    searchButton = tk.Button(button_frame, text="Search")
+    searchButton.pack(side="left", padx="5", pady="5")
+
+    viewFrame = tk.Frame(root)
+    viewFrame.pack(side="top", fill="both", expand=True)
+
+    inventoryView = ttk.Treeview(viewFrame, columns=("Card Name", "Card Number", "Quantity", "Location"), show="headings" )
+
+    inventoryView.heading("Card Name", text="Card Name")
+    inventoryView.heading("Card Number", text="Card Number")
+    inventoryView.heading("Quantity", text="Quantity")
+    inventoryView.heading("Location", text="Location")
+
+    inventoryView.column("Card Name", width="200")
+    inventoryView.column("Card Number", width="120")
+    inventoryView.column("Quantity", width="20")
+    inventoryView.column("Location", width="100")
+
+    inventoryView.pack(fill="both", expand=True, padx=10, pady=10)
+    root.mainloop()
