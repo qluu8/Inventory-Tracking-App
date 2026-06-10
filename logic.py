8b766fbd9c4c0a0b02d84a12d8f569ec896bd072
qluu8 <116929783+qluu8@users.noreply.github.com>
2026-06-09 19:51:51 -0700

    Removed console commands from main.py

--- logic.py
+++ logic.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from database import get_connection
 
 def addCard():
@@ -77,84 +76,4 @@ def showAll():
     else:
         print("No cards found.")
         
-=======
-from database import get_connection
-
-def addCard():
-    cardName = input("Enter card name: ")
-    cardNumber = input("Enter card number: ")
-    cardQ = input("Enter quantity of card(s): ")
-    loc = input("Enter location of card(s): ")
-    
-    connect = get_connection()
-    cursor = connect.cursor()
-    
-    cursor.execute("""
-        INSERT INTO cards (cardN, cardID, quantity, location)
-        VALUES (?, ?, ?, ?)
-                   """, (cardName, cardNumber, cardQ, loc))
-    
-    connect.commit()
-    connect.close()
-    print("Card added successfully.")
-    
-
-def searchCard():
-    choice = input("Enter name of card: ")
-    
-    connect = get_connection()
-    cursor = connect.cursor()
-    
-    cursor.execute("""
-        SELECT * FROM cards WHERE cardN LIKE ?
-                   """, ("%" + choice + "%",))
-    
-    results = cursor.fetchall()
-    
-    if results:
-        for card in results:
-            print(card)
-    else:
-        print("No card with that name found.")
-        
-    connect.close()
-
-def deleteCard():
-    showAll()
-    name = input("Enter name of card: ")
-    number = input("Enter the number of the card: ")
-    
-    connect = get_connection()
-    cursor = connect.cursor()
-    
-    cursor.execute("""
-        DELETE FROM cards WHERE cardN = ? AND cardID = ?
-                   """, (name, number))
-
-    connect.commit()
-    
-    if cursor.rowcount > 0:
-        print("Card deleted successfuly")
-    else:
-        print("No matching card found.")
-        
-    connect.close()
-    
-def showAll():
-    connect = get_connection()
-    cursor = connect.cursor()
-    
-    cursor.execute("""
-        SELECT * FROM cards
-                   """)
-    
-    results = cursor.fetchall()
-    
-    if results:
-        for card in results:
-            print(card)
-    else:
-        print("No cards found.")
-        
->>>>>>> 4840dccbc12baeb629cba64b6e6fdf0c455ebe63
     connect.close()
